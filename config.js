f4fae7ab1cd40da28af7895a8aa3ffde5e856263
touchafrika <mayikathabo@gmail.com>
2020-11-08 11:29:32 +0200

    removed config file

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://touchafrika:Ntombizodwa13@cluster0.aehfh.gcp.mongodb.net/merng?retryWrites=true&w=majority",
-  SECRET_KEY: "SOME VERY SECRET_KEY",
-};
